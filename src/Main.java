9dadf73b5a5f181c7969fdf9c3d0aaf538da59c6
lumu-xavier <xavierfrancis391.@gmail.com>
2025-11-03 21:47:36 +0300

    commit1
    In the new version, I have deleted lines 6, 10, 18 and 19 that show information about the sp object of the SalesPerson Class.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@
 public class Main {
     public static void main(String[] args) {
         Manager mgr = new Manager("M001","LUMU", 500000, "Managerial", 20, "Ethenz");
-        SalesPerson sp = new SalesPerson("M002", "XAVIER", 400000, "SALES", 20, "Kampala");
 
         System.out.println("EMPLOYEE DETAILS");
         mgr.displayDetails();
-        sp.displayDetails();
 
         System.out.println();
 
@@ -15,7 +13,6 @@ public class Main {
         double managerAnnualBonus = mgr.computeCompensation(0.25);
         System.out.println("Manager Annual Bonus: " + managerAnnualBonus);
 
-        double salesExceedTaget = sp.computeCompensation(0.30);
-        System.out.println("Sales Exceeding Target Bonus: " + salesExceedTaget);
+       
     }
 }
